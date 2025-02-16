13216b7c68a1d5541ba5415fb52944d942124413
Anastasia-Kiseleva12 <pm15.kiseleva1@gmail.com>
2025-02-16 16:05:55 +0300

    доработать

--- Task1_Game/Task1_Game.cpp
+++ Task1_Game/Task1_Game.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <thread>
 #include <mutex>
-#include <map>
 
 using namespace std;
 
@@ -20,7 +19,6 @@ private:
     vector<vector<char>> board;
     char player;
     char opponent;
-    int noCaptureMoves = 0;
 
     void initializeBoard() {
         board = vector<vector<char>>(BOARD_SIZE, vector<char>(BOARD_SIZE, ' '));
@@ -43,7 +41,7 @@ private:
         }
         cout << "  -----------------" << endl;
         for (int i = 0; i < BOARD_SIZE; ++i) {
-            int rowNumber = (player == 'B') ? BOARD_SIZE - i : i + 1;
+            int rowNumber = (player == 'B') ? i + 1 : 8 - i;
             cout << rowNumber << "|";
             for (int j = 0; j < BOARD_SIZE; ++j) {
                 if (player == 'B') {
@@ -62,68 +60,116 @@ private:
         int dx = move.to.first - move.from.first;
         int dy = move.to.second - move.from.second;
 
-        if (piece == 'W' && dx != -1) return false;
-        if (piece == 'B' && dx != 1) return false;
-        if (abs(dy) != 1) return false;
         if (board[move.to.first][move.to.second] != ' ') return false;
 
-        return true;
-    }
-
-    void makeMove(Move move) {
-        char piece = board[move.from.first][move.from.second];
-        if (isValidMove(move, piece)) {
-            board[move.to.first][move.to.second] = piece;
-            board[move.from.first][move.from.second] = ' ';
+        if (abs(dx) == 1 && abs(dy) == 1) {
+            if ((piece == 'W' && dx == -1) || (piece == 'B' && dx == 1) || piece == 'X' || piece == 'Y') {
+                return true;
+            }
         }
+
+        if (abs(dx) == 2 && abs(dy) == 2) {
+            int capX = (move.from.first + move.to.first) / 2;
+            int capY = (move.from.second + move.to.second) / 2;
+            if (board[capX][capY] == opponent || board[capX][capY] == (opponent == 'B' ? 'Y' : 'X')) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    void opponentMove() {
-        lock_guard<mutex> lock(mtx);
-        int direction = (opponent == 'W') ? -1 : 1;
+    bool canCapture(char piece) {
+        int directions[4][2] = { {-1, -1}, {-1, 1}, {1, -1}, {1, 1} };
         for (int i = 0; i < BOARD_SIZE; ++i) {
             for (int j = 0; j < BOARD_SIZE; ++j) {
-                if (board[i][j] == opponent) {
-                    Move move1 = { {i, j}, {i + direction, j + 1} };
-                    Move move2 = { {i, j}, {i + direction, j - 1} };
-                    if (isValidMove(move1, opponent)) {
-                        makeMove(move1);
-                        return;
-                    }
-                    else if (isValidMove(move2, opponent)) {
-                        makeMove(move2);
-                        return;
+                if (board[i][j] == piece) {
+                    for (auto dir : directions) {
+                        int x = i + dir[0], y = j + dir[1];
+                        int x2 = i + 2 * dir[0], y2 = j + 2 * dir[1];
+                        if (x2 >= 0 && x2 < BOARD_SIZE && y2 >= 0 && y2 < BOARD_SIZE &&
+                            (board[x][y] == opponent || board[x][y] == (opponent == 'B' ? 'Y' : 'X')) &&
+                            board[x2][y2] == ' ') {
+                            return true;
+                        }
                     }
                 }
             }
         }
+        return false;
+    }
+
+    void makeMove(Move move) {
+        char piece = board[move.from.first][move.from.second];
+        board[move.to.first][move.to.second] = piece;
+        board[move.from.first][move.from.second] = ' ';
+
+        if (abs(move.to.first - move.from.first) == 2) {
+            int capX = (move.from.first + move.to.first) / 2;
+            int capY = (move.from.second + move.to.second) / 2;
+            board[capX][capY] = ' ';
+        }
+
+        if (piece == 'W' && move.to.first == 0) board[move.to.first][move.to.second] = 'X';
+        if (piece == 'B' && move.to.first == BOARD_SIZE - 1) board[move.to.first][move.to.second] = 'Y';
+    }
+
+    void opponentMove() {
+        bool captured;
+        do {
+            captured = false;
+            Move bestMove;
+            for (int i = 0; i < BOARD_SIZE; ++i) {
+                for (int j = 0; j < BOARD_SIZE; ++j) {
+                    if (board[i][j] == opponent) {
+                        int dir = (opponent == 'B') ? 1 : -1;
+                        Move possibleMoves[] = {
+                            {{i, j}, {i + dir, j + 1}},
+                            {{i, j}, {i + dir, j - 1}}
+                        };
+                        for (Move m : possibleMoves) {
+                            if (m.to.first >= 0 && m.to.first < BOARD_SIZE &&
+                                m.to.second >= 0 && m.to.second < BOARD_SIZE &&
+                                isValidMove(m, opponent)) {
+                                makeMove(m);
+                                captured = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        } while (captured);
     }
 
 public:
     CheckersGame(char playerColor) : player(playerColor) {
         opponent = (player == 'W') ? 'B' : 'W';
         initializeBoard();
+        if (player == 'B') {
+            opponentMove();
+        }
+        printBoard();
     }
 
     void play() {
-        if (player == 'B') {
-            opponentMove();
-            printBoard();
-        }
-
         while (true) {
             string moveStr;
             cout << "Введите ход (например, A6 B5): ";
             getline(cin, moveStr);
             if (moveStr.length() != 5) continue;
 
-            int fromX = moveStr[1] - '1';
+            int fromX = 8 - (moveStr[1] - '0');
             int fromY = moveStr[0] - 'A';
-            int toX = moveStr[4] - '1';
+            int toX = 8 - (moveStr[4] - '0');
             int toY = moveStr[3] - 'A';
 
             Move move = { {fromX, fromY}, {toX, toY} };
             if (isValidMove(move, player)) {
+                if (canCapture(player) && abs(toX - fromX) != 2) {
+                    cout << "Вы обязаны выполнить взятие!" << endl;
+                    continue;
+                }
                 makeMove(move);
                 printBoard();
                 opponentMove();
